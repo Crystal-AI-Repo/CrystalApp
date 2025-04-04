cb0afe1aa064704255901a8b0229a9832a49b9d6
LovelyCat <1252251361@qq.com>
2025-04-05 03:17:00 +0800

    initial commit

---
+++ CrystalAppServer/src/test/kotlin/com/lovelycatv/ai/crystalapp/CrystalAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lovelycatv.ai.crystalapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrystalAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
